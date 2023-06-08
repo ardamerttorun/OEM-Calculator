a165a80f96ef61ac2cd348f5fa1eca4f73e18451
Arda Torun <ardamerttorun@gmail.com>
2023-06-08 15:38:29 +0300

    Yapı

---
+++ app.js
@@ -0,0 +1,40 @@
+// Storage Controller
+const StorageController=(function(){
+
+
+
+
+})();
+
+
+
+// Product Controller
+const ProductController=(function () {
+
+    // Private
+
+    //Public
+    return{
+
+    }
+
+})();
+
+
+// UI Controller
+
+const UIController=(function(){
+
+
+
+})();
+
+
+// App Controller
+const AppController=(function(ProductCtrl,UICtrl){
+
+ProductCtrl.
+
+
+})(ProductController,UIController);
+
