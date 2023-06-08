4c92fbf2189f978084001f63242be182f9a67131
Arda Torun <ardamerttorun@gmail.com>
2023-06-08 15:56:50 +0300

    List

--- app.js
+++ app.js
@@ -1,40 +1,80 @@
 // Storage Controller
-const StorageController=(function(){
-
-
-
-
-})();
-
-
+const StorageController = (function () {})();
 
 // Product Controller
-const ProductController=(function () {
+const ProductController = (function () {
+  // Private
+  const Product = function (id, name, price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  };
 
-    // Private
+  const data = {
+    products: [
+      { id: 0, name: "Monitör", price: 100 },
+      { id: 1, name: "Ram", price: 30 },
+      { id: 2, name: "KeyBoard", price: 10 },
+    ],
+    selectedProduct: null,
+    totalPrice: 0,
+  };
 
-    //Public
-    return{
-
-    }
-
+  //Public
+  return {
+    getProducts: function () {
+      return data.products;
+    },
+    getData: function () {
+      return data;
+    },
+  };
 })();
-
 
 // UI Controller
 
-const UIController=(function(){
+const UIController = (function () {
 
+    const Selectors={
+        productsList:"#item-list"
+    }
 
+  return {
+    createProductList: function (products) {
+      let html = ``;
 
+      products.forEach((prd) => {
+        html += `<tr>
+                <td>${prd.id+1}</td>
+                <td>${prd.name}</td>
+                <td>${prd.price}$</td>
+                <td class="text-right">
+                  <button type="submit" class="btn btn-warning btn-sm">
+                    <i class="far fa-edit"></i>
+                  </button>
+                </td>
+              </tr>
+                        `;
+      });
+
+      document.querySelector(Selectors.productsList).innerHTML = html;
+    },
+    getSelectors:function(){
+        return Selectors;
+    }
+  };
 })();
 
-
 // App Controller
-const AppController=(function(ProductCtrl,UICtrl){
+const App = (function (ProductCtrl, UICtrl) {
+  return {
+    init: function () {
+      console.log("Starting App...");
+      const products = ProductCtrl.getProducts();
 
-ProductCtrl.
+      UICtrl.createProductList(products);
+    },
+  };
+})(ProductController, UIController);
 
-
-})(ProductController,UIController);
-
+App.init();
